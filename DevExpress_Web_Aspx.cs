2b6031050dc16f2b754757d92be61bfb6ed49578
JongSSung <jury2531@naver.com>
2022-12-27 11:25:38 +0900

    modify

--- DevExpress_Web_Aspx.cs
+++ DevExpress_Web_Aspx.cs
@@ -1,18 +1,13 @@
-- 글자수 제한설정
-
-```csharp
-MaxLength="" 사용
-
+// - 글자수 제한설정
+// MaxLength="" 사용
 <dx:ASPxTextBox runat="server" ID="aPxTBxMatType" 
 ClientIDMode="Static" CssClass="txt_search" **MaxLength="10"**>
 </dx:ASPxTextBox>
-```
 
-- 숫자는 우측정렬
-    - 글자수 제한설정 할필요없음
-    - 3자리 마다 “ , “ 설정
 
-```csharp
+// - 숫자는 우측정렬
+//     - 글자수 제한설정 할필요없음
+//     - 3자리 마다 “ , “ 설정
 <script>
 
 				function OnUserInput(s, e) {
@@ -30,14 +25,12 @@ ClientIDMode="Static" CssClass="txt_search" **MaxLength="10"**>
 </script>
 
 <ClientSideEvents UserInput="OnUserInput" />
-```
 
-- 날짜 디폴트 오늘로 설정
 
-```csharp
+// - 날짜 디폴트 오늘로 설정
+
 <ClientSideEvents Init="function(s,e){ s.SetDate(new Date());}" />  사용
 
 <dx:ASPxDateEdit ID="aPxDTEtBuyDate" runat="server" ClientIDMode="Static" CssClass="txt_search">
-**<ClientSideEvents Init="function(s,e){ s.SetDate(new Date());}" />** 
+<ClientSideEvents Init="function(s,e){ s.SetDate(new Date());}" />
  </dx:ASPxDateEdit>
-```
